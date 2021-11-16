c8c46099d14227a327a5887925319ec1f8e8e340
Michel Schanen <mschanen@anl.gov>
2021-11-16 16:21:55 -0600

    allowscalar error

--- benchmark.jl
+++ benchmark.jl
@@ -1,7 +1,9 @@
+
+using CUDA
 using ExaAdmm
 using LazyArtifacts
 
-const INSTANCES_DIR = joinpath(artifact"ExaData", "ExaData")
-const case = joinpath(INSTANCES_DIR, "ACTIVSg70K.raw")
+const case = joinpath("matpower", "data", "case_ACTIVSg70k.m")
 
 env, mod = ExaAdmm.solve_acopf(case; rho_pq=1e1, rho_va=1e3, outer_iterlim=20, inner_iterlim=20, scale=1e-4, tight_factor=0.99, use_gpu=true)
+CUDA.@profile env, mod = ExaAdmm.solve_acopf(case; rho_pq=1e1, rho_va=1e3, outer_iterlim=3, inner_iterlim=20, scale=1e-4, tight_factor=0.99, use_gpu=true)
