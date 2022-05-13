4c7ee2494ebcc5635b9ace688ee85fbfb0afd579
Bowen Li <bowen.li@homes02.cels.anl.gov>
2022-05-12 23:14:09 -0500

    reconfigure tron

--- src/other_test/tron_qp.jl
+++ src/other_test/tron_qp.jl
@@ -51,7 +51,7 @@
 #     # return ExaTron.createProblem(n, l, u, nnz(P), eval_f, eval_g, eval_h) #param
 # end
 
-function build_QP(A::Matrix{Float64}, b::Array{Float64, 1}, l::Array{Float64, 1}, u::Array{Float64, 1})
+function build_QP_SP(A::Matrix{Float64}, b::Array{Float64, 1}, l::Array{Float64, 1}, u::Array{Float64, 1})
     # Random.seed!(1)
     # m = 0
 
@@ -64,7 +64,7 @@ function build_QP(A::Matrix{Float64}, b::Array{Float64, 1}, l::Array{Float64, 1}
     q = b
     # u =   1. * rand(n)
     # l = - 100. * rand(n)
-    Iz, Jz, vals = findnz(P)
+    Iz, Jz, vals = findnz(P) #does not see symmetric record n*n
 
     eval_f(x) = 0.5 * dot(x, P, x) + dot(q, x) #obj watch out for 1/2 
 
@@ -76,17 +76,83 @@ function build_QP(A::Matrix{Float64}, b::Array{Float64, 1}, l::Array{Float64, 1}
 
     function eval_h(x, mode, rows, cols, obj_factor, lambda, values)
         if mode == :Structure
-            for i in 1:nnz(P)
+            for i in 1:nnz(P) #does not symmetric
                 rows[i] = Iz[i]
                 cols[i] = Jz[i]
             end
+            # for j=1:n
+            #     for i in j:n 
+            #         nz += 1
+            #         rows[nz] = i
+            #         cols[nz] = j
+            #     end
+            # end
         else
             copy!(values, vals)
+            # for j=1:n
+            #     for i in j:n 
+            #         nz += 1
+            #         values[nz] = A[i, j]
+            #     end
+            # end
         end
     end
 
-    # return ExaTron.createProblem(n, l, u, nnz(P), eval_f, eval_g, eval_h; :tol=> 1e-6, :matrix_type => :Dense, :max_minor => 200, :frtol => 1e-12) #with param 
     return ExaTron.createProblem(n, l, u, nnz(P), eval_f, eval_g, eval_h) #original without param
+end
+
+
+function build_QP_DS(A::Matrix{Float64}, b::Array{Float64, 1}, l::Array{Float64, 1}, u::Array{Float64, 1})
+    # Random.seed!(1)
+    # m = 0
+
+    @assert size(A,1)==size(A,2)==length(b)==length(l)==length(u)
+    @assert l<=u
+    @assert issymmetric(A) #symmetric
+    
+    n=length(b)
+    # P = sparse(A) #make A sparse 
+    # q = b
+    # u =   1. * rand(n)
+    # l = - 100. * rand(n)
+    # Iz, Jz, vals = findnz(P)
+
+    eval_f(x) = 0.5 * dot(x, A, x) + dot(b, x) #obj watch out for 1/2 
+
+    function eval_g(x, g)
+        fill!(g, 0)
+        mul!(g, A, x)
+        g .+= b
+    end
+
+    function eval_h(x, mode, rows, cols, obj_factor, lambda, values)
+        if mode == :Structure
+            # for i in 1:nnz(P)
+            #     rows[i] = Iz[i]
+            #     cols[i] = Jz[i]
+            # end
+            nz = 1
+            for j=1:n
+                for i in j:n 
+                    rows[nz] = i
+                    cols[nz] = j
+                    nz += 1
+                end
+            end
+        else
+            # copy!(values, vals)
+            nz = 1
+            for j=1:n
+                for i in j:n 
+                    values[nz] = A[i, j]
+                    nz += 1
+                end
+            end
+        end
+    end
+    #with Youngdae's Param
+    return ExaTron.createProblem(n, l, u, Int64((n+1)*n/2), eval_f, eval_g, eval_h; :tol=> 1e-6, :matrix_type => :Dense, :max_minor => 200, :frtol => 1e-12)  
+    # return ExaTron.createProblem(n, l, u, Int64((n+1)*n/2), eval_f, eval_g, eval_h) #original without param
 end
 
 #test     
@@ -145,31 +211,51 @@ end
 # end
 
 
-function trontest2(;n=2) ##test for build_QP and solve_QP_Ipopt
+function trontest2(;n=8) ##test for build_QP and solve_QP_Ipopt
     A=rand(n,n)
-    A=A+transpose(A)
+    A=A+transpose(A) #symmetric 
     b=rand(n)
     u=rand(n)
     l=u.-10
+    
+    time_ds = @timed begin
+    tr_ds= build_QP_DS(A,b,l,u)
+    status_tron_ds = ExaTron.solveProblem(tr_ds)
+    end
+    
+    time_sp = @timed begin
+    tr_sp = build_QP_SP(A,b,l,u)
+    status_tron_sp = ExaTron.solveProblem(tr_sp)
+    end
 
-    tr= build_QP(A,b,l,u)
-    status_tron = ExaTron.solveProblem(tr)
-    val,obj,status=solve_QP_Ipopt(A,b,l,u)
+    time_ip = @timed val,obj,status=solve_QP_Ipopt(A,b,l,u)
+
+    println("time tron dense = ", time_ds.time)
+    println("time tron sparse = ", time_sp.time)
+    println("time ipopt = ", time_ip.time)
     
 
     ## this test easily fails when n is large 
-    @testset "QP sol: Tron vs Ipopt" begin
+    @testset "QP sol: Tron (sparse) vs Ipopt" begin
     #   @test status_tron == :Solve_Succeeded
-      @test norm(val - tr.x)≈0 atol=1e-3
-      @test obj≈tr.f atol=1e-3 
+      @test norm(val - tr_sp.x)≈0 atol=1e-3
+      @test obj≈tr_sp.f atol=1e-3 
     #   @test norm(val - tr.x)≈0 rtol=1e-6
     #   @test obj≈tr.f rtol=1e-6 
     end
 
-    return tr, val, obj, status, status_tron
+    @testset "QP sol: Tron (dense) vs Ipopt" begin
+        #   @test status_tron == :Solve_Succeeded
+          @test norm(val - tr_ds.x)≈0 atol=1e-3
+          @test obj≈tr_ds.f atol=1e-3 
+        #   @test norm(val - tr.x)≈0 rtol=1e-6
+        #   @test obj≈tr.f rtol=1e-6 
+        end
+
+    return val, obj, status, tr_ds, status_tron_ds, tr_sp, status_tron_sp
 end
 #one_line_test 
-#tr,val,obj,status,status_tron = ExaAdmm.trontest2(n=5)
+#val, obj, status, tr_ds, status_tron_ds, tr_sp, status_tron_sp = ExaAdmm.trontest2(n=5)
 
 # function trontest2()
 # @testset "1-d QP" begin
