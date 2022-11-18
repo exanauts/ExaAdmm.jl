a406d0cfb09ce661559be6aeac999e46c4fe7225
Michel Schanen <mschanen@anl.gov>
2022-11-18 16:23:51 -0500

    Fixing norm for AMD

--- src/utils/utilities_ka.jl
+++ src/utils/utilities_ka.jl
@@ -17,3 +17,27 @@ end
         c[tx] = a[tx] - b[tx]
     end
 end
+@kernel function norm_kernel(::Val{n}, x,
+                    y
+                    ) where {n}
+    tx = @index(Local, Linear)
+    bx = @index(Group, Linear)
+
+    _x = @localmem Float64 (n,)
+    _x[tx] = x[tx]
+    @synchronize
+
+    v = ExaTron.dnrm2(n, _x, 1, tx)
+    if bx == 1 && tx == 1
+        y[1] = v
+    end
+    @synchronize
+
+end
+function LinearAlgebra.norm(x, device)
+    y = KAArray{Float64}(1, device)
+    n = length(x)
+    wait(norm_kernel(device,n)(Val{n}(), x, y,ndrange=(n,),dependencies=Event(device)))
+    ret = y |> Array
+    return ret[1]
+end
