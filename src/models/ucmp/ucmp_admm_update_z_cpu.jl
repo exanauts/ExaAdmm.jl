e3e1f1612601e6d07c151e73e6d49ad4af6848ff
wzhangw <weiqizhang64@gmail.com>
2022-11-28 22:06:00 +0000

    implement admm_update_z for ucmp

--- src/models/ucmp/ucmp_admm_update_z_cpu.jl
+++ src/models/ucmp/ucmp_admm_update_z_cpu.jl
@@ -6,5 +6,21 @@ function admm_update_z(
     mod::UCMPModel{Float64,Array{Float64,1},Array{Int,1},Array{Float64,2}},
     device::Nothing=nothing
 )
+    admm_update_z(env, mod.mpmodel, device)
+    # update z for binary variables
+    ngen = mod.mpmodel.models[1].grid_data.ngen
+    par = env.params
+    uc_sol = mod.uc_solution
+    uc_sol.z_curr .= (-(uc_sol.lz .+ uc_sol.l_curr .+ uc_sol.rho .* (uc_sol.u_curr .- uc_sol.v_curr))) ./ (par.beta .+ uc_sol.rho)
+
+    # update z for v ramp variables
+    vr_sols = mod.vr_solution
+    for i=2:mod.mpmodel.len_horizon
+        vr_sol = vr_sols[i]
+        v_curr = @view uc_sol.v_curr[:, 3*i-2]
+        vr_sol.z_curr .= (-(vr_sol.lz .+ vr_sol.l_curr .+ vr_sol.rho .* (vr_sol.u_curr .- v_curr))) ./ (par.beta .+ vr_sol.rho)
+    end
+
+    # TODO: add time update later
     return
 end
