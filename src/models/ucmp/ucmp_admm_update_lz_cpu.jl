9d08c43efd21dbb79fc0cc06b5be712c6c18e3ca
wzhangw <weiqizhang64@gmail.com>
2022-11-30 02:20:08 +0000

    implement lz update for ucmp cpu

--- src/models/ucmp/ucmp_admm_update_lz_cpu.jl
+++ src/models/ucmp/ucmp_admm_update_lz_cpu.jl
@@ -6,5 +6,17 @@ function admm_update_lz(
     mod::UCMPModel{Float64,Array{Float64,1},Array{Int,1},Array{Float64,2}},
     device::Nothing=nothing
 )
+    admm_update_lz(env, mod.mpmodel, device)
+
+    par = env.params
+    uc_sol, vr_sols = mod.uc_solution, mod.vr_solution
+    # update lz for uc binary solution
+    uc_sol.lz .= max.(-par.MAX_MULTIPLIER, min.(par.MAX_MULTIPLIER, uc_sol.lz .+ (par.beta .* uc_sol.z_curr)))
+
+    # update lz for v ramp solution
+    for i=2:mod.mpmodel.len_horizon
+        vr_sol = vr_sols[i]
+        vr_sol.lz .= max.(-par.MAX_MULTIPLIER, min.(par.MAX_MULTIPLIER, vr_sol.lz .+ (par.beta .* vr_sol.z_curr)))
+    end
     return
 end
