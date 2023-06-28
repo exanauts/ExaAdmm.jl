d071be9789b98ec0b361e68243d8ade186b36af8
Kibaek Kim <kibaek.k@gmail.com>
2023-06-28 17:23:50 -0500

    cleanup

--- src/models/ucmp/ucmp_auglag_generator_kernel_cpu.jl
+++ src/models/ucmp/ucmp_auglag_generator_kernel_cpu.jl
@@ -244,16 +244,6 @@ function ucmp_auglag_generator_kernel(
             #     println("xi = [", param[21,I], ", ", param[22,I], ", ", param[23,I], ", ", param[24,I], ", ", param[25,I], ", ", param[26,I], "]")
             # end
 
-            if I == 2 && t > 1
-                @printf("param = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n",
-                    param[1,I], param[2,I], param[3,I], param[4,I], param[5,I], param[6,I], param[7,I], param[8,I], param[9,I], param[10,I],
-                    param[11,I], param[12,I], param[13,I], param[14,I], param[15,I], param[16,I], param[17,I], param[18,I], param[19,I], param[20,I],
-                    param[21,I], param[22,I], param[23,I], param[24,I], param[25,I], param[26,I], param[27,I], param[28,I], param[29,I], param[30,I],
-                    param[31,I], param[32,I], param[33,I], param[34,I], param[35,I], param[36,I], param[37,I], param[38,I], param[39,I], param[40,I],
-                    param[41,I]
-                )
-            end
-
             ExaTron.solveProblem(tron)
             x .= tron.x
             avg_tron_minor += tron.minor_iter
@@ -268,10 +258,6 @@ function ucmp_auglag_generator_kernel(
             if t > 1
                 cvios[1] = x[1] - x[3] - param[34,I]*x[7] - param[35,I]*x[5] - x[8]
                 cvios[2] = x[1] - x[3] + param[36,I]*x[4] + param[37,I]*x[6] + x[9]
-                if I == 2
-                    @printf("cvios = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n", cvios[1], cvios[2], cvios[3], cvios[4], cvios[5], cvios[6])
-                    @printf("x = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n", x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13])
-                end
             end
             # if I == 2
             #     @show x
