ae2270ccd97726f99342a73a8b0dae5d075db6ff
wzhangw <weiqizhang64@gmail.com>
2023-06-28 04:49:09 +0000

    log cleanup

--- src/models/ucmp/ucmp_auglag_generator_kernel_gpu.jl
+++ src/models/ucmp/ucmp_auglag_generator_kernel_gpu.jl
@@ -142,59 +142,8 @@ function ucmp_auglag_generator_kernel(
         it = 0
         terminate = false
 
-        # if I == 2 && tx == 1
-        #     @cuprintf("x_gpu = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n", x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13])
-        #     # Everything up to here is consistent
-        # end
-
-        # if I == 2 && tx == 1
-        #     @cuprintf("param = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-        #         param[1,I], param[2,I], param[3,I], param[4,I], param[5,I], param[6,I], param[7,I], param[8,I]
-        #     )
-        #     @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-        #         param[9,I], param[10,I], param[11,I], param[12,I], param[13,I], param[14,I], param[15,I], param[16,I]
-        #     )
-        #     @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-        #         param[17,I], param[18,I], param[19,I], param[20,I], param[21,I], param[22,I], param[23,I], param[24,I]
-        #     )
-        #     @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-        #         param[25,I], param[26,I], param[27,I], param[28,I], param[29,I], param[30,I], param[31,I], param[32,I]
-        #     )
-        #     @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n",
-        #         param[33,I], param[34,I], param[35,I], param[36,I], param[37,I], param[38,I], param[39,I], param[40,I], param[41,I]
-        #     )
-        # param is checked to be the same between cpu and gpu
-        # end
-
-        # if I == 2 && tx == 1
-        #     @cuprintf("xl_gpu = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n", xl[1], xl[2], xl[3], xl[4], xl[5], xl[6], xl[7], xl[8], xl[9], xl[10], xl[11], xl[12], xl[13])
-        #     @cuprintf("xu_gpu = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n", xu[1], xu[2], xu[3], xu[4], xu[5], xu[6], xu[7], xu[8], xu[9], xu[10], xu[11], xu[12], xu[13])
-        # end
-
         while !terminate
             it += 1
-            # if I == 2 && tx == 1
-            #     @cuprintln("mu = [", param[8,I], ", ", param[9,I], ", ", param[10,I], ", ", param[11,I], ", ", param[12,I], ", ", param[13,I], "]")
-            #     @cuprintln("xi = [", param[21,I], ", ", param[22,I], ", ", param[23,I], ", ", param[24,I], ", ", param[25,I], ", ", param[26,I], "]")
-            # end
-
-            if I == 2 && tx == 1 && t > 1
-                @cuprintf("param = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-                    param[1,I], param[2,I], param[3,I], param[4,I], param[5,I], param[6,I], param[7,I], param[8,I]
-                )
-                @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-                    param[9,I], param[10,I], param[11,I], param[12,I], param[13,I], param[14,I], param[15,I], param[16,I]
-                )
-                @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-                    param[17,I], param[18,I], param[19,I], param[20,I], param[21,I], param[22,I], param[23,I], param[24,I]
-                )
-                @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e ",
-                    param[25,I], param[26,I], param[27,I], param[28,I], param[29,I], param[30,I], param[31,I], param[32,I]
-                )
-                @cuprintf("%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n",
-                    param[33,I], param[34,I], param[35,I], param[36,I], param[37,I], param[38,I], param[39,I], param[40,I], param[41,I]
-                )
-            end
 
             status, minor_iter = ucmp_tron_generator_kernel(t, n, 500, 200, 1e-6, scale, x, xl, xu, param, c2, c1, c0, baseMVA)
             # Check the termination condition.
@@ -208,19 +157,10 @@ function ucmp_auglag_generator_kernel(
                 if t > 1
                     cvios[1] = x[1] - x[3] - param[34,I]*x[7] - param[35,I]*x[5] - x[8]
                     cvios[2] = x[1] - x[3] + param[36,I]*x[4] + param[37,I]*x[6] + x[9]
-                    if I == 2
-                        # @cushow cvios[2]
-                        @cuprintf("cvios = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n", cvios[1], cvios[2], cvios[3], cvios[4], cvios[5], cvios[6])
-                        @cuprintf("x = [%10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e %10.6e]\n", x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13])
-                    end
                 end
             end
 
             CUDA.sync_threads()
-            # if I == 2 && tx == 1
-            #     @cuprintln("x = [", x[1], ", ", x[2], ", ", x[3], ", ", x[4], ", ", x[5], ", ", x[6], ", ", x[7], ", ", x[8], ", ", x[9], ", ", x[10], ", ", x[11], ", ", x[12], ", ", x[13], "]")
-            #     @cuprintln("cvios = [", cvios[1], ", ", cvios[2], ", ", cvios[3], ", ", cvios[4], ", ", cvios[5], ", ", cvios[6], "]")
-            # end
 
             terminate = true
             for i in _st:6
@@ -243,7 +183,6 @@ function ucmp_auglag_generator_kernel(
                 end
             end
             if it >= max_auglag
-                # @cuprintln("inner auglag reach max iteration at time $(t) for generator $(I)")
                 terminate = true
             end
             CUDA.sync_threads()
@@ -267,9 +206,6 @@ function ucmp_auglag_generator_kernel(
         end
 
         CUDA.sync_threads()
-        # if I == 2 && tx == 1
-        #     @cuprintln("x = [", x[1], ", ", x[2], ", ", x[3], ", ", x[4], ", ", x[5], ", ", x[6], ", ", x[7], ", ", x[8], ", ", x[9], ", ", x[10], ", ", x[11], ", ", x[12], ", ", x[13], "]")
-        # end
     end
     return
 end
